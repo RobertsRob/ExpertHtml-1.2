f90d0d8d3732e5543e5ffa80f2581ffb6a3a6e70
RobertsRob <83211059+RobertsRob@users.noreply.github.com>
2022-05-19 12:34:24 +0300

    lololo

--- jd_static.js
+++ jd_static.js
@@ -99,7 +99,5 @@ function evs(){
     }else{
     if (cikPavIzr > 7,5 && cikPavIzr < 10){
         document.getElementById("diagr_pr").style.borderTop = "120px solid green";
-    }else{
-        document.getElementById("diagr_pr").style.borderTop = "7px solid red";
     }}}}
 }
